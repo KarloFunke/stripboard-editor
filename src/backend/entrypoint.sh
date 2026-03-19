81913926eda3cd92511edee8ed04fafd3f0fe279
KarloFunke <karl.funke@indocu.tech>
2026-03-19 19:59:44 +0100

    reducing worker count

--- src/backend/entrypoint.sh
+++ src/backend/entrypoint.sh
@@ -13,5 +13,5 @@ python manage.py collectstatic --noinput
 echo "Starting gunicorn..."
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --workers 3 \
+    --workers 2 \
     --timeout 120
